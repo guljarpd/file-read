fc7e1e6488319128558eb7bb416ac87f8e0b3a06
Guljar Prasad <34159121+guljarpd@users.noreply.github.com>
2024-11-18 16:57:18 +0530

    added config

---
+++ config.js
@@ -0,0 +1,46 @@
+const syntaxConfig = {
+    javascript: {
+        singleLineComment: '//',
+        multiLineCommentStart: '/*',
+        multiLineCommentEnd: '*/'
+    },
+    python: {
+        singleLineComment: '#',
+        multiLineCommentStart: '"""',
+        multiLineCommentEnd: '"""'
+    },
+    java: {
+        singleLineComment: '//',
+        multiLineCommentStart: '/*',
+        multiLineCommentEnd: '*/'
+    },
+    typescript: {
+        singleLineComment: '//',
+        multiLineCommentStart: '/*',
+        multiLineCommentEnd: '*/'
+    },
+    elixir: {
+        singleLineComment: '#',
+        multiLineCommentStart: '"""',
+        multiLineCommentEnd: '"""'
+    },
+    php: {
+        singleLineComment: '//',
+        multiLineCommentStart: '/*',
+        multiLineCommentEnd: '*/'
+    },
+    c: {
+        singleLineComment: '//',
+        multiLineCommentStart: '/*',
+        multiLineCommentEnd: '*/'
+    },
+    cpp: {
+        singleLineComment: '//',
+        multiLineCommentStart: '/*',
+        multiLineCommentEnd: '*/'
+    },
+};
+
+module.exports = {
+    syntaxConfig,
+};
